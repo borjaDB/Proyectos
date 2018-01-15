28632bcee633775107acb72acb29dfafab88a347
borja981 <33958932+borja981@users.noreply.github.com>
2018-01-15 17:24:51 +0000

    Create Stateless.java

---
+++ JavaEE/EJB/SessionBeans/Stateless.java
@@ -0,0 +1,16 @@
+package Stateless;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+
+@Stateles
+@LocalBean
+
+public class BeanLocal {
+
+    public int suma(int numero) {  
+        return numero+10; ;   
+    }   
+    
+}
